346708461136b8b1f2f7ab2464f3ab84f68f1367
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-04-19 21:53:32 -0300

    Resizing, shapes and cuda concepts with pytorch

--- main.py
+++ main.py
@@ -19,3 +19,48 @@ arr = np.random.rand(3, 4)
 tns = torch.from_numpy(arr) # Preserves the original array's type, (float64) in this case
 print(arr)
 print(tns)
+
+tns1 = torch.ones(2, 3)
+tns0 = torch.zeros(4, 5)
+tnsr = torch.randn(3, 3)
+
+print(tns1)
+print(tns0)
+print(tnsr)
+
+#Tensor to numpy
+print(type (arr))
+arr = tnsr.data.numpy()
+print(type (arr))
+
+print(tnsr)
+tnsr[0, 2] = -10
+print(" ")
+print(tnsr)
+
+print(" ")
+print(tnsr[:, 2])
+
+print(tnsr[0, 2].size())
+
+tns = tnsr[0:2, :]
+print(tns.shape)
+print(tns1.shape)
+
+print(tns+tns1)
+print(torch.mm(tns1, tns.T))
+
+tns = torch.randn(2,2,3)
+print(tns)
+
+print(tns.size())
+print(tns.view(tns.size(), -1))
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+print(device)
+
+tns = tns.to(device)
+print(tns)
